ac451603c933d2db6386e20055fa9a8e29b2bfb9
MoryFabrice <mory.fabrice@hotmail.fr>
2024-05-16 16:08:07 +0200

    first commit

---
+++ controllers/index.php
@@ -0,0 +1,10 @@
+<?php
+
+$template1 = "views/nav";
+$template2 = "views/header";
+$template3 = "views/portfolio";
+$template4 = "views/aPropos";
+$template5 = "views/contact";
+$template6 = "views/modal";
+$template7 = "views/footer";
+include("./views/index.phtml");
---
+++ index.php
@@ -0,0 +1,2 @@
+<?php
+include('./controllers/index.php');
