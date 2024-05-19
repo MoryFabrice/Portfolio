9413af85adc28e055e50393dd06415161ff7cf7d
MoryFabrice <mory.fabrice@hotmail.fr>
2024-05-19 23:01:08 +0200

    Rajout des skills
    Plusieurs petits changement
    Mais surtout l'ajout des skills et du trie qui permet que tout soit plus clairs et moins fouillis

--- controllers/index.php
+++ controllers/index.php
@@ -3,8 +3,9 @@
 $template1 = "views/nav";
 $template2 = "views/header";
 $template3 = "views/portfolio";
-$template4 = "views/aPropos";
-$template5 = "views/contact";
-$template6 = "views/modal";
-$template7 = "views/footer";
+$template4 = "views/skills";
+$template5 = "views/aPropos";
+$template6 = "views/contact";
+$template7 = "views/modal";
+$template8 = "views/footer";
 include("./views/index.phtml");
