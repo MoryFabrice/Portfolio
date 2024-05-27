98893ea4a82b8a18b32d6cf6cb4d9a168e9b4115
MoryFabrice <mory.fabrice@hotmail.fr>
2024-05-27 13:41:36 +0200

    Plusieurs modifs
    Changement des images
    Mise en forme des textes et ajout des langages utilisés

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
 if (array_key_exists("action", $_GET)) {
 
     switch ($_GET['action']) {
-            //si index.php?action=categorie ==alors il va appeler le controlleurs categorie.php qui va de son cote communiquer avec le model et faire l affichage dans la page(view)
+            //si index.php?action=contact ==alors il va appeler le controlleurs contact.php
+            // qui va de son cote communiquer avec le model et faire l affichage dans la page(view)
         case "contact":
             include 'controllers/contact.php';
             break;
